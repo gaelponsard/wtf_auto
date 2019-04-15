c8e66e5a12268d10325c43ff51f7fd99e3bf3195
gaelponsard <g.ponsard@simplon-charleville.fr>
2019-04-15 20:46:40 +0200

    décor et auto en place

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>wtf auto</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+SCORE : <span id="info">0</span>
+
+<div id="WTF Auto">
+
+  <img id="fond1" class="fond" src="assets/route.png">
+
+  <img id="fond2" class="fond" src="assets/route.png">
+
+  <img id="vr" src="assets/vb.png">
+
+  <img id="vb" src="assets/vr.png">
+
+</div>
+
+<audio preload="auto" id="son"><source src="beep.mp3" type="audio/mp3"><source src="beep.ogg" type="audio/ogg"></audio>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+
+function deplace()
+
+{
+
+  $('.fond').animate({
+
+    right: '-=360'
+
+  },
+
+  1000,
+
+  'linear',
+
+  function(){
+
+    $('.fond').css('right', 0);
+
+    deplace();
+
+  });
+
+}
+
+  <script>
+</body>
+</html>
